fc82de9c36cf8b52496ca4a76ffec3c9372cb314
Gaz <gaz1978@yahoo.co.uk>
2023-01-29 00:05:59 +0000

    Changed Beater to Champion

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -29,7 +29,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World Beater";
+    String someone = "World Champion";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
